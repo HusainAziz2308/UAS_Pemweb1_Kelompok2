5e5fe15b8fda3368952d57d83c05f1f3b610c19a
alvin616 <nukhialvinrhmdni@gmail.com>
2025-12-17 13:28:05 +0700

    halaman tentang

---
+++ pages/tentang.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="admin/assets/css/style.css">
+    <link rel="shortcut icon" href="admin/assets/img/favicon1.png" type="image/png">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
