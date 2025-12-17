e05517596fc1f312b6e493aa67a9f2ea0f6f4b80
HusainAziz2308 <husainazizalrosyid@gmail.com>
2025-12-17 14:08:55 +0700

    perbaikan style dan fav icon

--- pages/tentang.php
+++ pages/tentang.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="admin/assets/css/style.css">
-    <link rel="shortcut icon" href="admin/assets/img/favicon1.png" type="image/png">
+    <link rel="stylesheet" href="../admin/assets/css/style.css">
+    <link rel="shortcut icon" href="../admin/assets/img/favicon1.png" type="image/png">
     <title>Tentang | Ruang Kopi</title>
 </head>
 <body>
