05951e2473c71fb05987fc51dfdcc32f7b6e3439
HusainAziz2308 <husainazizalrosyid@gmail.com>
2025-12-20 23:42:54 +0700

    Pathfix #12 (session admin fix 4)

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -35,7 +35,7 @@ if (!$query) {
 <body>
     <a href="logout.php" class="logout">Logout</a>
     <h1>Dashboard Admin</h1>
-    <p>Selamat datang <b><?= $_SESSION['nama_admin']; ?></b>! Selamat Bekerja.... </p>
+    <p>Selamat datang Kak <b><?= $_SESSION['nama_admin']; ?></b>! Selamat Bekerja.... </p>
     <a href="laporan_penjualan.php">📊 Laporan Penjualan</a>
     <br><br>    
     <a href="tambah_kopi.php">+ Tambah Kopi</a> 
